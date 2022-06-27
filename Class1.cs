139fb45e8d0c978b07ec61163ab0442859a64ffd
Mari-Anne Eerikäinen <marianne.eerikainen@gmail.com>
2022-06-27 13:52:48 +0300

    added study is late method

--- Class1.cs
+++ Class1.cs
@@ -4,5 +4,22 @@ namespace ClassLibraryDateMethods
 {
     public class Class1
     {
+        public bool FutureDate(DateTime date)
+        {
+            DateTime now = DateTime.Today;
+            TimeSpan isFuture = now - date;
+            int days = Convert.ToInt32(isFuture.Days);
+
+            if (days < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
     }
 }
